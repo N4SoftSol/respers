103c3465368d07914e280035f82d85688910d5b4
Naveen Paulraj <Naveen.Paulraj@n4softsol.com>
2026-09-13 22:08:36 -0400

    v1.4 update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   authBaseUrl: 'https://authjsb.restapi.n4softsol.com',
   apiBaseUrl: 'https://persjsb.restapi.n4softsol.com',
   appInfo: {
-    gitUpdate: '1.2',
+    gitUpdate: '1.4',
     application: 'apps-angular-respers',
     profiles: ['prd'],
   },
