0844625da28d5e27fc5b90929a668e87c772ec4c
Naveen Paulraj <Naveen.Paulraj@n4softsol.com>
2026-09-15 09:04:15 -0400

    Completed github actions repository runner

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   authBaseUrl: 'https://authjsb.restapi.n4softsol.com',
   apiBaseUrl: 'https://persjsb.restapi.n4softsol.com',
   appInfo: {
-    GitUpdate: '6.0',
+    gitUpdate: '6.0',
     application: 'apps-angular-respers',
     profiles: ['prd'],
   },
