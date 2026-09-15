9038b8e8effa827566644b611f9648b4a35a047a
Naveen Paulraj <Naveen.Paulraj@n4softsol.com>
2026-09-15 13:23:10 -0400

    ci: fix workflow labels for organization runner

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   // apiBaseUrl: 'https://persjsb.restapi.n4softsol.com', // Spring Boot API base URL
 
   appInfo: {
-    GitUpdate: '6.0',
+    gitUpdate: '6.0',
     application: 'dev-apps-angular-respers',
     profiles: ['dev'], // Set to dev for the development environment
   },
